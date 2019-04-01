3b8a83ffe81f6aaa94cd0856d634e3e51a15b1f1
Romeo <misinicialesbray@gmail.com>
2019-04-01 14:01:29 -0600

    Se realizo la opción de ver saldo

--- perfil.aspx
+++ perfil.aspx
@@ -5,13 +5,45 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title></title>
+    <title>REGISTRO TU BANCO S.A.</title>
 </head>
-<body>
-    <form id="form1" runat="server">
-        <div>
-            <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
-        </div>
-    </form>
+    <style type="text/css">
+body {
+ background-image: url(banco1.jpg);
+	  background-position: center center;	  
+	  background-repeat: no-repeat;
+	background-attachment: fixed;
+	background-size: cover;
+}
+
+#mail {
+	width: 100%;
+	position: fixed;
+}
+</style>
+        <form id="form1" runat="server">
+        <center><img src="texto2.png"></center>
+   
+
 </body>
+           <center> 
+               <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+
+               <br />
+               <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+               <br />
+               <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+               <br />
+               <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label>
+               <br />
+               <br />
+               <br />
+               <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="CONSULTAR SALDO" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+               <asp:Label ID="Label5" runat="server" Text="Label"></asp:Label>
+               <br />
+
+           </center>
+</form>
+
 </html>
--- perfil.aspx.cs
+++ perfil.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
 
 namespace Practica3
 {
@@ -11,12 +14,94 @@ namespace Practica3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            TextBox1.Text = Session["Codigo_Usuario"].ToString();
+            if (Session["Codigo_Usuario"].ToString().Length == 0)
+            {
+                MessageBox.Show("Usuario Sin Permisos");
+                Response.Redirect("Login.aspx");
+            }
+            else {
+                Label5.Text = "";
+                //Obteniendo nombre y usuario
+                SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog = Practica3_AYD1;Trusted_Connection=true;");
+                con.Open();
+                SqlCommand com = new SqlCommand(); // Create a object of SqlCommand class
+                com.Connection = con; //Pass the connection object to Command
+                com.CommandType = CommandType.StoredProcedure; // We will use stored procedure.
+                com.CommandText = "VerUsuario"; //Stored Procedure Name
+                com.Parameters.Add("@codigo_usuario", SqlDbType.NVarChar).Value = Session["Codigo_Usuario"].ToString();
+                com.ExecuteNonQuery();
+                String Usuario = "";
+                String Nombre_Usuario = "";
+                SqlDataReader reader = com.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Nombre_Usuario = reader.GetString(0);
+                        Usuario = reader.GetString(1);
+                        break;
+                    }
+                }
+
+                ///Obteniendo Numero de Cuenta
+                ///
+                SqlConnection conx = new SqlConnection("Data Source=.;Initial Catalog = Practica3_AYD1;Trusted_Connection=true;");
+                conx.Open();
+                SqlCommand comx = new SqlCommand(); // Create a object of SqlCommand class
+                comx.Connection = conx; //Pass the connection object to Command
+                comx.CommandType = CommandType.StoredProcedure; // We will use stored procedure.
+                comx.CommandText = "VerCuenta"; //Stored Procedure Name
+                comx.Parameters.Add("@codigo_usuario", SqlDbType.NVarChar).Value = Session["Codigo_Usuario"].ToString();
+                comx.ExecuteNonQuery();
+                int cuenta = 0;
+                SqlDataReader readerx = comx.ExecuteReader();
+                if (readerx.HasRows)
+                {
+                    while (readerx.Read())
+                    {
+                        cuenta = readerx.GetInt32(0);
+                        break;
+                    }
+                }
+
+
+                Label1.Text = "Codigo Usuario " + Session["Codigo_Usuario"].ToString();
+                Label2.Text = "Usuario " + Usuario;
+                Label3.Text = "Nombre Usuario " + Nombre_Usuario;
+                Label4.Text = "Numero Cuenta " + cuenta;
+            }
+           
         }
         
 
         protected void TextBox1_TextChanged(object sender, EventArgs e)
         {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conx = new SqlConnection("Data Source=.;Initial Catalog = Practica3_AYD1;Trusted_Connection=true;");
+            conx.Open();
+            SqlCommand comx = new SqlCommand(); // Create a object of SqlCommand class
+            comx.Connection = conx; //Pass the connection object to Command
+            comx.CommandType = CommandType.StoredProcedure; // We will use stored procedure.
+            comx.CommandText = "VerSaldo"; //Stored Procedure Name
+            comx.Parameters.Add("@codigo_usuario", SqlDbType.NVarChar).Value = Session["Codigo_Usuario"].ToString();
+            comx.ExecuteNonQuery();
+            decimal SALDO = 0;
+            SqlDataReader readerx = comx.ExecuteReader();
+            if (readerx.HasRows)
+            {
+                while (readerx.Read())
+                {
+                    SALDO = readerx.GetDecimal(0);
+                    break;
+                }
+            }
+
+
+            Label5.Text = "Q. " + SALDO;
 
         }
     }
